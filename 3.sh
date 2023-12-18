c7ad97171d167e63216c0f4d0cd7b882ffe1d637
ezra <ezrayeneneh1992@gmail.com>
2023-12-18 04:22:16 -0800

    what is wrong

--- 3.sh
+++ 3.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 #install nginx
 
-sudo su
 sudo useradd -r nginx
 sudo chown -R nginx /etc/nginx
 
 
 
-sed -i "s/#user www-data/user nginx/" /etc/nginx/nginx.conf
-chmod 644 /etc/nginx/nginx.conf
-sed -i 's/^User=.*$/User=nginx/g' /lib/systemd/system/nginx.service
-sed -i 's/^Group=.*$/Group=nginx/g' /lib/systemd/system/nginx.service
+sudo sed -i "s/#user www-data/user nginx/" /etc/nginx/nginx.conf
+sudo chmod 644 /etc/nginx/nginx.conf
+sudo sed -i 's/^User=.*$/User=nginx/g' /lib/systemd/system/nginx.service
+sudo sed -i 's/^Group=.*$/Group=nginx/g' /lib/systemd/system/nginx.service
 if ! grep -q "^User=nginx" /lib/systemd/system/nginx.service; then
-	    echo "User=nginx" >> /lib/systemd/system/nginx.service
+	    sudo echo "User=nginx" >> /lib/systemd/system/nginx.service
 fi
 
 if ! grep -q "^Group=nginx" /lib/systemd/system/nginx.service; then
-	    echo "Group=nginx" >> /lib/systemd/system/nginx.service
+	    sudo echo "Group=nginx" >> /lib/systemd/system/nginx.service
 fi
 sudo setcap 'cap_net_bind_service=+ep' /usr/sbin/nginx
 sudo apt-get install authbind   # For Debian/Ubuntu
