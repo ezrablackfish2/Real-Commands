ee0e4d1c3fc85a3ee23c43a34ac441fa698b14b7
ezra <ezrayeneneh1992@gmail.com>
2023-12-18 22:46:47 -0800

    waw

--- ssl.sh
+++ ssl.sh
@@ -7,3 +7,6 @@ sudo systemctl stop nginx
 sudo service haproxy stop
 sudo certbot certonly --standalone -d hlb8qg2p9.tech -d www.hlb8qg2p9.tech  
 cat /etc/letsencrypt/live/hlb8qg2p9.tech/privkey.pem >> /etc/letsencrypt/live/hlb8qg2p9.tech/fullchain.pem
+rm /etc/haproxy/haproxy.cfg
+touch /etc/haproxy/haproxy.cfg
+cat ./haproxy >> etc/haproxy/haproxy.cfg
