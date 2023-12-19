c80931f590775c963223135ceb10e2beccf7ca95
ezra <ezrayeneneh1992@gmail.com>
2023-12-19 06:07:56 -0800

    replica

---
+++ mysql_replica.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Set MySQL commands as a string
+
+MYSQL_COMMANDS=$(cat <<EOF
+CREATE USER 'replica_user'@'%' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION SLAVE ON "*" TO "replica_user"@"%";
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR "replica_user"@"%";
+SHOW GRANTS FOR 'holberton_user'@'localhost';
+EOF
+)
+
+# Pipe the MySQL commands to the MySQL command line
+echo "${MYSQL_COMMANDS}" | sudo mysql -u root -p
