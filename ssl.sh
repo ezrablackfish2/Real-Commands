aa8cbb43d434a177bbd319c3feabaf0ea9bdb254
ezra <ezrayeneneh1992@gmail.com>
2023-12-18 22:49:01 -0800

    wow wow

--- ssl.sh
+++ ssl.sh
@@ -6,7 +6,9 @@ sudo apt install certbot
 sudo systemctl stop nginx
 sudo service haproxy stop
 sudo certbot certonly --standalone -d hlb8qg2p9.tech -d www.hlb8qg2p9.tech  
-cat /etc/letsencrypt/live/hlb8qg2p9.tech/privkey.pem >> /etc/letsencrypt/live/hlb8qg2p9.tech/fullchain.pem
-rm /etc/haproxy/haproxy.cfg
-touch /etc/haproxy/haproxy.cfg
-cat ./haproxy >> /etc/haproxy/haproxy.cfg
+sudo cat /etc/letsencrypt/live/hlb8qg2p9.tech/privkey.pem >> /etc/letsencrypt/live/hlb8qg2p9.tech/fullchain.pem
+sudo rm /etc/haproxy/haproxy.cfg
+sudo touch /etc/haproxy/haproxy.cfg
+sudo cat ./haproxy >> /etc/haproxy/haproxy.cfg
+
+sudo service haproxy restart
