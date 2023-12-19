677837064ee60e492af58c9c7849720f0d3e2e89
ezra <ezrayeneneh1992@gmail.com>
2023-12-19 11:19:47 -0800

    yes

---
+++ mysql_web_01_replicate.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Set MySQL commands as a string
+
+sudo echo "log_bin = /var/log/mysql/mysql-bin.log
+server-id = 1">> /etc/mysql/mysql.conf.d/mysqld.cnf
+
+sudo service mysql restart
+sudo service mysql status
+
+MYSQL_COMMANDS=$(cat <<EOF
+SHOW MASTER STATUS;
+EOF
+)
+
+# Pipe the MySQL commands to the MySQL command line
+echo "${MYSQL_COMMANDS}" | sudo mysql -u root -p
--- mysql_web_02_replicate.sh
+++ mysql_web_02_replicate.sh
@@ -2,12 +2,15 @@
 # Set MySQL commands as a string
 
 sudo echo "log_bin = /var/log/mysql/mysql-bin.log
-server-id = 1" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+server-id = 2
+relay-log = /var/log/mysql/mysql-relay-bin.log
+binlog_db = tyrell_corp" >> /etc/mysql/mysql.conf.d/mysqld.cnf
+
 sudo service mysql restart
 sudo service mysql status
 
 MYSQL_COMMANDS=$(cat <<EOF
-SHOW MASTER STATUS;
+CHANGE MASTER TO MASTER_HOST='100.27.5.198', MASTER_USER='replica_user', MASTER_PASSWORD='tutu@12345', MASTER_LOG_FILE='', MASTER_LOG_POS='';
 EOF
 )
 
