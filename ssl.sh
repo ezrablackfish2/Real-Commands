ade2d9be0b85c8d31f16655ca040d3887f825d68
ezra <ezrayeneneh1992@gmail.com>
2023-12-18 22:39:11 -0800

    yes yes

--- ssl.sh
+++ ssl.sh
@@ -5,5 +5,5 @@ sudo apt update
 sudo apt install certbot
 sudo systemctl stop nginx
 sudo service haproxy stop
-sudo certbot certonly --standalone -d www.hlb8qg2p9.tech -d hlb8qg2p9.tech
+sudo certbot certonly --standalone -d hlb8qg2p9.tech -d www.hlb8qg2p9.tech  
 cat /etc/letsencrypt/live/hlb8qg2p9.tech/privkey.pem >> /etc/letsencrypt/live/hlb8qg2p9.tech/fullchain.pem
