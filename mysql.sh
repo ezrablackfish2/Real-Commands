62ee6bdf998e7365916f00e8b059088b6fe136d8
ezra <ezrayeneneh1992@gmail.com>
2023-12-19 03:17:53 -0800

    yes

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 #install msqyl
 
+sudo apt-key add signature.key
+sudo sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu bionic mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
 sudo apt-get update
-sudo apt-get install mysql-server-5.7
-mysql --version
-
+sudo apt-cache policy mysql-server
+sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*
