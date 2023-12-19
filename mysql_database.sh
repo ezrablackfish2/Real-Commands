c01f73e6947bbebb773d994f7b25e81f96461c8a
ezra <ezrayeneneh1992@gmail.com>
2023-12-19 05:52:50 -0800

    yes finished

---
+++ mysql_database.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Set MySQL commands as a string
+
+MYSQL_COMMANDS=$(cat <<EOF
+CREATE DATABASE tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE nexus6 ( id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR (255) );
+INSERT INTO nexus6 (name) VALUES ('leon');
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+USE tyrell_corp;
+SELECT * FROM nexus6;
+SHOW GRANTS FOR 'holberton_user'@'localhost';
+EOF
+)
+
+# Pipe the MySQL commands to the MySQL command line
+echo "${MYSQL_COMMANDS}" | sudo mysql -u root -p
