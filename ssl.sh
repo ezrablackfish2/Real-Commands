fa0ea20a47323cf4627a0e15216f1be6063c3074
ezra <ezrayeneneh1992@gmail.com>
2023-12-18 22:47:48 -0800

    wow

--- ssl.sh
+++ ssl.sh
@@ -9,4 +9,4 @@ sudo certbot certonly --standalone -d hlb8qg2p9.tech -d www.hlb8qg2p9.tech
 cat /etc/letsencrypt/live/hlb8qg2p9.tech/privkey.pem >> /etc/letsencrypt/live/hlb8qg2p9.tech/fullchain.pem
 rm /etc/haproxy/haproxy.cfg
 touch /etc/haproxy/haproxy.cfg
-cat ./haproxy >> etc/haproxy/haproxy.cfg
+cat ./haproxy >> /etc/haproxy/haproxy.cfg
