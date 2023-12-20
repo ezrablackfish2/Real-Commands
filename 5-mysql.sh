fbe071a3d2fdb8b2dc6cf671d4f26df11046a911
ezra <ezrayeneneh1992@gmail.com>
2023-12-19 23:46:48 -0800

    mysql 5.7

--- 5-mysql.sh
+++ 5-mysql.sh
@@ -2,6 +2,13 @@
 #install msqyl
 
 sudo apt-get update
-sudo apt-get install mysql-server
-sudo systemctl start mysql
-mysql -u root -p
+sudo apt install wget -y
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.12-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.12-1_all.deb
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B7B3B788A8D3785C
+sudo apt-get update
+sudo apt-cache policy mysql-server
+sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7* -y
+
+sudo service mysql restart
+sudo service mysql status
