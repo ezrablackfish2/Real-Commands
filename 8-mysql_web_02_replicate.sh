d09c735c289d998081a443b2fbe38cf4b4422166
ezra <ezrayeneneh1992@gmail.com>
2023-12-19 22:24:29 -0800

    yes

--- 8-mysql_web_02_replicate.sh
+++ 8-mysql_web_02_replicate.sh
@@ -14,6 +14,7 @@ sudo service mysql restart
 sudo service mysql status
 
 MYSQL_COMMANDS=$(cat <<EOF
+STOP REPLICA IO_THREAD FOR CHANNEL '';
 CHANGE MASTER TO MASTER_HOST='52.86.73.43', MASTER_USER='replica_user', MASTER_PASSWORD='projectcorrection280hbtn', MASTER_LOG_FILE='mysql-bin.000001', MASTER_LOG_POS=157;
 START SLAVE;
 SHOW SLAVE STATUS\G;
