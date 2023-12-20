82edbee4e790555395a483e87255bea5b069b866
ezra <ezrayeneneh1992@gmail.com>
2023-12-20 00:08:49 -0800

    yea man

--- 1-authorization.sh
+++ 1-authorization.sh
@@ -2,3 +2,8 @@
 # this iss to copy
 
 sudo cat ./ssh-pub-key > ~/.ssh/authorized_keys
+sudo cat ./ssh-pub-key > /home/ubuntu/.ssh/authorized_keys
+chmod 700 /home/ubuntu/.ssh
+chmod 600 /home/ubuntu/.ssh/authorized_keys
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/authorized_keys
