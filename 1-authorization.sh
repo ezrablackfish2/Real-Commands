2539b79f3da382819ae37f018ccea9a447ec9c15
ezra <ezrayeneneh1992@gmail.com>
2023-12-20 00:38:03 -0800

    overwrite

--- 1-authorization.sh
+++ 1-authorization.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # this iss to copy
 
-sudo cat ./ssh-pub-key >> ~/.ssh/authorized_keys
-sudo cat ./ssh-pub-key >> /home/ubuntu/.ssh/authorized_keys
+sudo bash -c 'cat ./ssh-pub-key >> ~/.ssh/authorized_keys'
+sudo bash -c 'cat ./ssh-pub-key >> /home/ubuntu/.ssh/authorized_keys'
 sudo chmod 700 /home/ubuntu/.ssh
 sudo chmod 600 /home/ubuntu/.ssh/authorized_keys
 sudo chmod 700 ~/.ssh
