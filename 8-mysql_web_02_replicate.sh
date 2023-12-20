04a8900d4a22adb20e6161627558b843fb92c924
ezra <ezrayeneneh1992@gmail.com>
2023-12-19 21:22:06 -0800

    order is everything

--- 8-mysql_web_02_replicate.sh
+++ 8-mysql_web_02_replicate.sh
@@ -13,6 +13,8 @@ sudo service mysql status
 
 MYSQL_COMMANDS=$(cat <<EOF
 CHANGE MASTER TO MASTER_HOST='100.27.5.198', MASTER_USER='replica_user', MASTER_PASSWORD='projectcorrection280hbtn', MASTER_LOG_FILE='mysql-bin.000002', MASTER_LOG_POS=157;
+START SLAVE;
+SHOW SLAVE STATUS\G;
 EOF
 )
 
